6195e704da07d3f46e3429bdd87276629b37073f
Davide Salerno <davideslrn97@gmail.com>
2020-08-23 13:18:55 +0200

    test Davide

--- src/test/TestStudDao.java
+++ src/test/TestStudDao.java
@@ -4,13 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.SQLException;
 
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import logic.dao.StudentDao;
 
+@TestMethodOrder(OrderAnnotation.class)
 public class TestStudDao {
 	// legal insert test
 	StudentDao studentDao = new StudentDao();
 	@Test
+	@Order(1)
 	void insertTest1() {		
 		try {
 			assertEquals(1, studentDao.insert("mailProva1@live.it", "password", "usernameProva1", "nomeProva1", "cognomeProva1",
@@ -21,6 +26,7 @@ public class TestStudDao {
 	}
 
 	@Test
+	@Order(2)
 	// illegal insert test in db mail attribute is primary key
 	void insertTest2() {
 		try {
@@ -33,6 +39,7 @@ public class TestStudDao {
 	}
 
 	@Test
+	@Order(3)
 	// illegal insert test in db phone attribute is VARCHAR(10)
 	void insertTest3() {
 		try {
@@ -45,11 +52,13 @@ public class TestStudDao {
 
 	// legal delete test
 	@Test
+	@Order(4)
 	void deleteTest1() {
 		assertEquals(1, studentDao.delete("Studente", "mailProva1@live.it", null));
 	}
 
 	@Test
+	@Order(5)
 	// illegal delete test of a not existing student
 	void deleteTest2() {
 		assertEquals(0, studentDao.delete("Studente", "mailProva3@live.it", null));
